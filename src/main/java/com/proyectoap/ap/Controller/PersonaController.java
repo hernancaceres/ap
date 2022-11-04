279930c655cab9ba4c26bc9034e61daba89f905a
hernancaceres <harinatha777@hotmail.com>
2022-11-04 09:37:25 -0300

    corrigiendo cors nuve4

--- src/main/java/com/proyectoap/ap/Controller/PersonaController.java
+++ src/main/java/com/proyectoap/ap/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://frontendap-bdefb.web.app/#/home/")
+@CrossOrigin(origins = "https://frontendap-bdefb.web.app/")
 //@CrossOrigin(origins = "https://hcfrontend.web.app")
 public class PersonaController {
 
