bee7818458dc73c1dfff1fed8d597e7804997f4d
hernancaceres <harinatha777@hotmail.com>
2022-11-04 08:43:30 -0300

    corrigiendo cors en la nuve

--- src/main/java/com/proyectoap/ap/Controller/ExperienciaController.java
+++ src/main/java/com/proyectoap/ap/Controller/ExperienciaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendap-bdefb.web.app")
 //@CrossOrigin(origins = "https://hcfrontend.web.app")
 public class ExperienciaController {
 
--- src/main/java/com/proyectoap/ap/Security/Controller/AuthController.java
+++ src/main/java/com/proyectoap/ap/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendap-bdefb.web.app")
 //@CrossOrigin(origins = "https://hcfrontend.web.app")
 public class AuthController {
 
