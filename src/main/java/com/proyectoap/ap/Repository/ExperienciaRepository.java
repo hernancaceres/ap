d98d8d1fc063f826909a2d8d027abf2258bc1f7f
hernancaceres <harinatha777@hotmail.com>
2022-11-03 21:34:59 -0300

    subiendo a clever-cloud

---
+++ src/main/java/com/proyectoap/ap/Repository/ExperienciaRepository.java
@@ -0,0 +1,14 @@
+package com.proyectoap.ap.Repository;
+
+import com.proyectoap.ap.Entity.Experiencia;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExperienciaRepository extends JpaRepository<Experiencia, Integer> {
+
+    public Optional<Experiencia> findByNombreE(String nombreE);
+
+    public boolean existsByNombreE(String nombreE);
+}
