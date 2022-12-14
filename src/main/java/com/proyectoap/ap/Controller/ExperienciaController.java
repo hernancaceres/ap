be21f84ad514f768ec0909619fb96589e7e29350
hernancaceres <harinatha777@hotmail.com>
2022-12-13 23:00:11 -0300

    resolviendocors en la nubexxx

--- src/main/java/com/proyectoap/ap/Controller/ExperienciaController.java
+++ src/main/java/com/proyectoap/ap/Controller/ExperienciaController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = { "https://frontendap-bdefb.web.app", "http://localhost:4200" }) 
+@CrossOrigin(origins = { "https://frontendap-bdefb.web.app/", "http://localhost:4200" }) 
 public class ExperienciaController {
 
     @Autowired
